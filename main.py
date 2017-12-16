7e378c033d8cb1865cbd0ca377da96be4c412e38
Aleksandr Gornostal <al.gornostal@gmail.com>
2017-12-16 17:15:13 +0200

    upgrade to manifest v2; added a new preference option: List item size

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.item.ExtensionSmallResultItem import ExtensionSmallResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
@@ -28,20 +29,21 @@ class EmojiExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
+        list_item_size = extension.preferences['list_item_size']
+        items_limit = 8 if list_item_size == 'large' else 20
+        ResultItem = ExtensionResultItem if list_item_size == 'large' else ExtensionSmallResultItem
         query = r"""SELECT
             em.name, em.code, em.icon, em.keywords,
             skt.code AS skt_code, skt.icon AS skt_icon
             FROM emoji AS em
             LEFT JOIN skin_tone AS skt ON skt.name = em.name AND tone = ?
             WHERE name_search LIKE ?
-            LIMIT 8"""
+            LIMIT ?"""
 
         search_term = ''.join(['%', event.get_argument().replace('%', ''), '%']) if event.get_argument() else None
         if not search_term:
             return RenderResultListAction([
-                ExtensionResultItem(icon=extension_icon,
-                                    name='Type in emoji name...',
-                                    on_enter=DoNothingAction())
+                ResultItem(icon=extension_icon, name='Type in emoji name...', on_enter=DoNothingAction())
             ])
 
         skin_tone = extension.preferences['skin_tone']
@@ -50,7 +52,7 @@ class KeywordQueryEventListener(EventListener):
             skin_tone = ''
 
         items = []
-        for row in conn.execute(query, [skin_tone, search_term]):
+        for row in conn.execute(query, [skin_tone, search_term, items_limit]):
             if row['skt_code']:
                 icon = row['skt_icon']
                 code = row['skt_code']
@@ -59,9 +61,7 @@ class KeywordQueryEventListener(EventListener):
                 code = row['code']
 
             name = ('%s %s' % (row['name'].capitalize(), code)).encode('utf8')
-            items.append(ExtensionResultItem(icon=icon,
-                                             name=name,
-                                             on_enter=CopyToClipboardAction(code)))
+            items.append(ResultItem(icon=icon, name=name, on_enter=CopyToClipboardAction(code)))
 
         return RenderResultListAction(items)
 
