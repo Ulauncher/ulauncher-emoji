886e44ac6bf8ed62076ed8de0124e1cfde4dfbf7
Rabin <mail@rjoshi.net>
2020-09-22 13:06:13 +0545

    add an option to set number of emojis to search

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ db_path = os.path.join(os.path.dirname(__file__), 'emoji.sqlite')
 conn = sqlite3.connect(db_path, check_same_thread=False)
 conn.row_factory = sqlite3.Row
 
+SEARCH_LIMIT_MIN = 2
+SEARCH_LIMIT_DEFAULT = 8
+SEARCH_LIMIT_MAX = 100
 
 class EmojiExtension(Extension):
 
@@ -28,13 +31,27 @@ class EmojiExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        query = r"""SELECT
-            em.name, em.code, em.icon, em.keywords,
-            skt.code AS skt_code, skt.icon AS skt_icon
-            FROM emoji AS em
-            LEFT JOIN skin_tone AS skt ON skt.name = em.name AND tone = ?
-            WHERE name_search LIKE ?
-            LIMIT 8"""
+        search_limit = extension.preferences['search_limit']
+
+        try:
+            search_limit = search_limit.strip()
+            search_limit = int(search_limit)
+
+            if search_limit < SEARCH_LIMIT_MIN:
+                search_limit = SEARCH_LIMIT_MIN
+            elif search_limit > SEARCH_LIMIT_MAX:
+                search_limit = SEARCH_LIMIT_MAX
+        except Exception as e:
+            search_limit = SEARCH_LIMIT_DEFAULT
+
+        query = 'SELECT \
+            em.name, em.code, em.icon, em.keywords, \
+            skt.code AS skt_code, skt.icon AS skt_icon \
+            FROM emoji AS em \
+            LEFT JOIN skin_tone AS skt ON skt.name = em.name AND tone = ? \
+            WHERE name_search LIKE ? \
+            LIMIT {}'.format(search_limit)
+
 
         search_term = ''.join(['%', event.get_argument().replace('%', ''), '%']) if event.get_argument() else None
         if not search_term:
